6e00d3be1e848c2400511b46129e5639eb4580fc
Dagurechaff <box153m@ya.ru>
2025-02-26 20:02:16 +0300

    Delete 2.py

--- 2.py
+++
@@ -1,9 +0,0 @@
-def F(w, x, y, z):
-	return not(not(x) or y) or (y == z) or not(w)
-print('w', 'x', 'y', 'z')
-for w in True, False:
-	for x in True, False:
-		for y in True, False:
-			for z in True, False:
-				if not(F(w, x, y, z)):
-					print(int(w), int(x), int(y), int(z), 0)
